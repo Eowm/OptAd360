e82b2df92824607f487eba2b70c357f4fb7c5ad1
Adam Nelip <Adam.Nelip@gmail.com>
2018-09-27 19:55:03 +0200

    Everything working, finished project

--- js/index.js
+++ js/index.js
@@ -1,25 +1,25 @@
 const accept = document.getElementById('accept');
 const cancel = document.getElementById('cancel');
 
-const MILISECONDS_IN_DAY = 24*60*60*1000;
-const addDays = (date, days) => new Date(date.getTime() + days*MILISECONDS_IN_DAY);
+const MILISECONDS_IN_DAY = 24 * 60 * 60 * 1000;
+const addDays = (date, days = 1) => new Date(date.getTime() + days * MILISECONDS_IN_DAY);
 
 const setCookie = (name, value) => {
     const date = new Date();
-    const expires = addDays(date, 1);
+    const expires = addDays(date);
     document.cookie = `${name} = ${value}; expires= + ${expires.toUTCString()}; path=/`;
 }
 
-if(!document.cookie.match(/^(.*;)?\s*newCookie\s*=\s*[^;]+(.*)?$/)) {
+if (!document.cookie.match(/^(.*;)?\s*newCookie\s*=\s*[^;]+(.*)?$/)) {
     document.getElementById('scroll').classList.add('scroll');
-    
+
     accept.addEventListener('click', event => {
         document.querySelector('.box').classList.add('hide');
         setCookie('newCookie', 'accept');
         document.getElementById('opacity').classList.add('show');
         document.getElementById('scroll').classList.remove('scroll');
     })
-    
+
     cancel.addEventListener('click', event => {
         document.querySelector('.box').classList.add('hide');
         setCookie('newCookie', 'cancel');
